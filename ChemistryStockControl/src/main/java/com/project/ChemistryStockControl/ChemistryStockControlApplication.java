e7661b4d8a9f39541422a7bf44e0d3a9c40cbe1a
eduardoAssuncao <eduardoasssousa@gmail.com>
2022-10-22 15:03:22 -0300

    commit com o projeto default

---
+++ ChemistryStockControl/src/main/java/com/project/ChemistryStockControl/ChemistryStockControlApplication.java
@@ -0,0 +1,13 @@
+package com.project.ChemistryStockControl;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChemistryStockControlApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChemistryStockControlApplication.class, args);
+	}
+
+}
---
+++ ChemistryStockControl/src/test/java/com/project/ChemistryStockControl/ChemistryStockControlApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.ChemistryStockControl;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChemistryStockControlApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
