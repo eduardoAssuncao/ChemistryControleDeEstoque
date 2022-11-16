5fddc890b4283f08ede39ac5b8f777ea2f6a9d79
“RegianaBarbosa” <regiana.cruz@cad.ifma.edu.br>
2022-11-16 18:27:19 -0300

    2 commit de R

--- ChemistryStockControl_frontend/js/frontend/main.js
+++ ChemistryStockControl_frontend/js/frontend/main.js
@@ -30,4 +30,22 @@ $(function (e) {
         },
       });
   });
+  $(function() {
+
+    $('.js-check-all').on('click', function() {
+  
+      if ( $(this).prop('checked') ) {
+        $('th input[type="checkbox"]').each(function() {
+          $(this).prop('checked', true);
+        })
+      } else {
+        $('th input[type="checkbox"]').each(function() {
+          $(this).prop('checked', false);
+        })
+      }
+    });
+  
+      
+  
+  });
   
