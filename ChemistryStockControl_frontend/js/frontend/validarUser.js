badb52679bf034cee00e214f8cc2eddf0b4b275b
eduardoAssuncao <eduardoasssousa@gmail.com>
2022-12-07 11:53:24 -0300

    tentativa de validar login

---
+++ ChemistryStockControl_frontend/js/frontend/validarUser.js
@@ -0,0 +1,41 @@
+const imatricula = document.querySelector('#floatingInput');
+const isenha = document.querySelector('#floatingPassword');
+
+const api_url = "http://localhost:8080/api/v1/users";
+
+let matricula = [];
+let nome = [];
+let email = [];
+let senha = [];
+
+function validar(data) {
+    
+    for (let r of data) {
+        let i = 0;
+        matricula = r.matricula;
+        nome = r.name;
+        email = r.email;
+        senha = r.senha;
+        if(imatricula.value == r.matricula && isenha.value == 55555){
+            console.log("Logando");
+            window.location.href="/ChemistryStockControl_frontend/html/adm/index.html";
+
+        }else{
+            console.log("Deu tudo errado");
+        }
+        i++;
+    }
+}
+
+form.addEventListener("submit", function(event) {
+    event.preventDefault();
+    async function getapi(url) {
+        const response = await fetch(url);
+    
+        var data = await response.json();
+        //info = data;
+        console.log(data);
+        validar(data);
+    }
+    getapi("http://localhost:8080/api/v1/users")
+});
