9285d327c3a32c07477c07ec7dd4bad822e21f35
eduardoAssuncao <eduardoasssousa@gmail.com>
2022-12-17 16:13:19 -0300

    Busca material parcial

---
+++ ChemistryStockControl_frontend/js/frontend/buscaMaterial.js
@@ -0,0 +1,28 @@
+const search_input = document.querySelector("#inputMat")
+
+search_input.addEventListener("keyup", function (e){
+    var search_item = e.target.value.toLowerCase()
+    var span_reagents_items = document.querySelectorAll("#reagentes tr .name")
+    
+    span_reagents_items.forEach(function(item){
+        console.log(item.textContent)
+        if(item.textContent.toLowerCase().indexOf(search_item) != -1){
+            item.closest("tr").style.display = "block";
+        }
+        else{
+            item.closest("tr").style.display = "none";
+        }
+    })
+
+    var span_glassware_items = document.querySelectorAll("#vidrarias tr .name")
+
+    span_glassware_items.forEach(function(item){
+        console.log(item.textContent)
+        if(item.textContent.toLowerCase().indexOf(search_item) != -1){
+            item.closest("tr").style.display = "block";
+        }
+        else{
+            item.closest("tr").style.display = "none";
+        }
+    })
+})
